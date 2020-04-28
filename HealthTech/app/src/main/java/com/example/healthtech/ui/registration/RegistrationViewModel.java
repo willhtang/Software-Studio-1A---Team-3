a40e6febd995b5c55f20aa0e449a0c56cf7f0fe6
willhtang <will.h.tang@gmail.com>
2020-04-28 11:36:30 +1000

    Created Registration Files

---
+++ HealthTech/app/src/main/java/com/example/healthtech/ui/registration/RegistrationViewModel.java
@@ -0,0 +1,11 @@
+package com.example.healthtech.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class RegistrationViewModel extends ViewModel {
+    public RegistrationViewModel(){
+
+    }
+}
