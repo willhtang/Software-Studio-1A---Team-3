acbbb75eb11a8406757ac2dcede1207738d0f769
willhtang <will.h.tang@gmail.com>
2020-05-12 12:41:09 +1000

    Fragment transaction code for RegistrationFragment

--- HealthTech/app/src/main/java/com/example/healthtech/ui/registration/RegistrationFragment.java
+++ HealthTech/app/src/main/java/com/example/healthtech/ui/registration/RegistrationFragment.java
@@ -8,14 +8,18 @@ import android.widget.RadioGroup;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.EditText;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.healthtech.R;
+import com.example.healthtech.ui.login.LoginFragement;
 
 public class RegistrationFragment extends Fragment {
     private RegistrationViewModel registrationViewModel;
@@ -25,7 +29,7 @@ public class RegistrationFragment extends Fragment {
     //OnCreateView Method
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+                             final ViewGroup container, Bundle savedInstanceState) {
         //Initialise View
         registrationViewModel = ViewModelProviders.of(this).get(RegistrationViewModel.class);
         View root = inflater.inflate(R.layout.registration_main, container, false);
@@ -44,6 +48,8 @@ public class RegistrationFragment extends Fragment {
         final RadioGroup rgUserType = (RadioGroup) root.findViewById(R.id.rgUserType);
         rbDoctor = (RadioButton) rgUserType.findViewById(R.id.radioDoctor);
         rbPatient = (RadioButton) rgUserType.findViewById(R.id.radioPatient);
+
+        final Button btCreate = (Button) root.findViewById(R.id.btnCreateAccount);
 
         //Listeners
         rgUserType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -71,7 +77,18 @@ public class RegistrationFragment extends Fragment {
                           break;
                   }*/
               }
-          });
+        });
+
+        btCreate.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(container.getId(), new LoginFragement());
+                fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
 
         /*
         registrationViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
