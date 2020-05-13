4a552d61c015057a6a958238600fb8c785fe3961
willhtang <will.h.tang@gmail.com>
2020-05-14 01:14:35 +1000

    Added base code for login fragment

--- HealthTech/app/src/main/java/com/example/healthtech/ui/login/LoginFragment.java
+++ HealthTech/app/src/main/java/com/example/healthtech/ui/login/LoginFragment.java
@@ -1,16 +1,31 @@
 package com.example.healthtech.ui.login;
 
-import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.healthtech.R;
 
 public class LoginFragment extends Fragment {
-    private LoginFragment LoginViewModel;
- /*
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    private LoginViewModel loginViewModel;
 
+    public static LoginFragment newLoginFragment() {
+        LoginFragment fragment = new LoginFragment();
+        return fragment;
+    }
+
+    //OnCreateView Method
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             final ViewGroup container, Bundle savedInstanceState) {
+        //Initialise View
+        loginViewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
+        View root = inflater.inflate(R.layout.login_home, container, false);
         return root;
     }
-*/
-
 }
