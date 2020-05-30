fd1c1fd7370f211c76139b0bd093c1d236dc99ea
13556042 <62224240+13556042@users.noreply.github.com>
2020-05-30 19:05:44 +1000

    appointments now show in the manager page
    Signed-off-by: 13556042 <62224240+13556042@users.noreply.github.com>

--- HealthTech/app/src/main/java/com/example/healthtech/ui/appointment/ManageAppointmentFragment.java
+++ HealthTech/app/src/main/java/com/example/healthtech/ui/appointment/ManageAppointmentFragment.java
@@ -4,20 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 
+import com.example.healthtech.Account;
+import com.example.healthtech.Appointment;
 import com.example.healthtech.R;
 
-public class ManageAppointmentFragment extends Fragment {
+import java.util.ArrayList;
+
+public class ManageAppointmentFragment extends ListFragment {
     public static ManageAppointmentFragment newInstance(){ return new ManageAppointmentFragment(); }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View root = inflater.inflate(R.layout.fragment_manage_appointments, container, false);
-
         return root;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AppointmentAdapter adapter = new AppointmentAdapter(getActivity(), Account.active.getAppointments());
+        this.setListAdapter(adapter);
     }
 }
